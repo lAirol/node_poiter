292dcdfd11e38ea9774a93188f7ce3606e2a2eb2
lAirol <114059019+lAirol@users.noreply.github.com>
2024-07-08 13:11:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const WebSocket = require('ws');
 const fs = require('fs');
 
-const hostname = 'lairol.github.io';
+const hostname = '127.0.0.1';
 const port = 5500;
 
 const server = http.createServer((req, res) => {
